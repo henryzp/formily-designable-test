491f5178ec0816f1999969b80dc6b452122db28f
henryzp <Henry_zp@163.com>
2022-10-10 23:23:05 +0800

    feat: 简化写法

--- src/pages/playground/source/common/IconSetter/index.tsx
+++ src/pages/playground/source/common/IconSetter/index.tsx
@@ -1,37 +1,11 @@
 import { Field, ObjectField, observer, useField } from '@formily/react';
 import { Input } from 'antd';
-import React, { useState } from 'react';
 
-interface IIconSetter {
-  value: {
-    type?: string;
-    name?: string;
-  };
-  onChange?: (value: any) => void;
-}
-
-function IconSetter(props: IIconSetter) {
-  const { value = { type: 'ICON' }, onChange } = props;
-
-  const filed = useField;
-
-  const [inputValue, setInputValue] = useState(() => {
-    return value.name || '';
-  });
-
-  const handleChange = (ev) => {
-    const value = ev.target.value;
-    setInputValue(value);
-    onChange &&
-      onChange({
-        type: 'ICON',
-        name: value,
-      });
-  };
-
+function IconSetter() {
   return (
     <>
-      <Input value={inputValue} onChange={handleChange} />
+      <Field name="type" value="ICON" hidden />
+      <Field name="name" component={[Input]} />
     </>
   );
 }
